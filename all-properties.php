c3daa7e8186b7a8d2f278e4da920c8769bcbb759
ddroy9090-ind <dev@reliantsurveyors.com>
2025-08-24 01:18:23 +0530

    Show estimated commission for properties

--- all-properties.php
+++ all-properties.php
@@ -19,7 +19,12 @@ $result = $conn->query("SELECT * FROM properties ORDER BY id DESC");
                     </div>
 
                     <?php if ($result->num_rows > 0): ?>
-                        <?php while ($property = $result->fetch_assoc()): ?>
+                        <?php while ($property = $result->fetch_assoc()):
+                            $commission = 0;
+                            if (!empty($property['starting_price'])) {
+                                $commission = floatval(str_replace(',', '', $property['starting_price'])) * 0.02;
+                            }
+                        ?>
                             <div class="col-lg-4">
                                 <div class="propertiesList">
                                     <div class="property-card">
@@ -38,20 +43,26 @@ $result = $conn->query("SELECT * FROM properties ORDER BY id DESC");
                                                     </h5>
                                                 </div>
                                                 <h5 class="property-name"><?= $property['project_name'] ?? '-'; ?></h5>
-                                                <div class="property-info">
-                                                    <div class="location">
-                                                        <img src="assets/icons/location.png" alt="" width="15">
-                                                        <span><?= $property['location'] ?? '-'; ?></span>
-                                                    </div>
-                                                    <div class="room-details">
-                                                        <span><img src="assets/icons/area.png" alt=""><?= $property['starting_area'] ?? '-'; ?></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+                                                  <div class="property-info">
+                                                      <div class="location">
+                                                          <img src="assets/icons/location.png" alt="" width="15">
+                                                          <span><?= $property['location'] ?? '-'; ?></span>
+                                                      </div>
+                                                      <div class="room-details">
+                                                          <span><img src="assets/icons/area.png" alt=""><?= $property['starting_area'] ?? '-'; ?></span>
+                                                      </div>
+                                                  </div>
+                                                  <div class="property-commission">
+                                                      <small>Commission (2%):
+                                                          <img src="assets/icons/dirham.svg" alt="">
+                                                          <?= number_format($commission, 2); ?>
+                                                      </small>
+                                                  </div>
+                                              </div>
+                                          </a>
+                                      </div>
+                                  </div>
+                              </div>
                         <?php endwhile; ?>
                     <?php else: ?>
                         <div class="col-12">
