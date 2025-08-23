4361d26cb74fd9b82e02d2831b68f5daac94db3c
ddroy9090-ind <dev@reliantsurveyors.com>
2025-08-24 00:26:30 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "channel_portal";
+$host = "localhost";  // check in hPanel > Databases; usually 'localhost'
+$user = "u431421769_channel_portal";
+$pass = "K#ng@9090";
+$db   = "u431421769_portal_cms";
 
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$conn->set_charset('utf8mb4'); // good practice
 ?>
-
